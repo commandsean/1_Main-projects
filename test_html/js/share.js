e9b5a16c5b9511b313aeadc62dc2d6c85a540066
commandsean <105042417+commandsean@users.noreply.github.com>
2022-08-06 23:06:16 +0900

    Initial commit

---
+++ test_html/js/share.js
@@ -0,0 +1,31 @@
+const url = 'https://soongmoon-carrer-html.netlify.app/';
+
+function setShare() {
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '대학 적합 학과 검사 ';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+  Kakao.Share.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+      },
+    },
+
+    buttons: [
+      {
+        title: '결과 확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+        },
+      },
+    ],
+  });
+}
